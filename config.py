8dbdaa7396e60179d8ea829d571cc7fc4f39f84e
sergeguiswa150-jpg <sergeguiswa150@gmail.com>
2026-06-28 14:08:19 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,18 @@
 import os
 
-# Ton Token sera récupéré depuis les variables d'environnement sur Render
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-# Ton ID numérique (récupéré via @userinfobot)
 ADMIN_ID = 6269793768
 
 BOT_NAME = "MrTech237"
 
-# Moyens de paiement affichés après le choix du pack
-PAYMENT_DETAILS = """
-💳 *Moyens de paiement acceptés :*
+PAYMENT_METHODS = """
+💳 Moyens de paiement acceptés :
 
-• *MTN Mobile Money* : +237XXXXXXXXX
-• *Orange Money* : +237XXXXXXXXX
-• *Crypto (USDT TRC20)* : `TON_ADRESSE_ICI`
-• *PayPal* : ton@email.com
-
-_Veuillez envoyer une capture d'écran du paiement une fois terminé._
+• MTN Mobile Money
+• Orange Money
+• PayPal
+• USDT
+• Bitcoin
+• Virement bancaire
 """
