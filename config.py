a07a1718c73001a77326ef73d88bbce9cbf4dd29
sergeguiswa150-jpg <sergeguiswa150@gmail.com>
2026-06-28 15:07:17 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,11 @@ import os
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
+if not TOKEN:
+    raise ValueError(
+        "La variable d'environnement TELEGRAM_TOKEN est absente."
+    )
+
 ADMIN_ID = 6269793768
 
 BOT_NAME = "MrTech237"
